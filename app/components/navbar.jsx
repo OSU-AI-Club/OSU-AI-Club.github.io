cb1130a8048e74547f8afb862d6cc788edaf9e35
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2024-02-08 18:35:55 -0500

    navbar updates

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
+
 export default function Navbar(props) {
     return (
         <nav class="absolute top-0 left-0 right-0">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8">
                 <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AI CLUB LOGO</span>
+                    <Image src="/logos/AI_Logo_Final.png" width="64" height="64" />
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
