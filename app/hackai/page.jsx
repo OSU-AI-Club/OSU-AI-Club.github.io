7b30c58c88ab513820fa94cc3f44b96c96eb7673
Benjamin-Lampe <lampemen@gmail.com>
2024-02-08 20:24:01 -0500

    Prototype footer.

--- app/hackai/page.jsx
+++ app/hackai/page.jsx
@@ -1,9 +1,12 @@
+import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 
 export default function HackAI() {
   return (
     <main className="">
       <Navbar homepage={false} />
+      <div class="min-h-screen bg-blue-300"></div>
+      <Footer></Footer>
     </main>
   );
 }
