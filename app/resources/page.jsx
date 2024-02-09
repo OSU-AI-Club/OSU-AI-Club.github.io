4d748b9969b6da1ddec750deaaa07d3350a3b622
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2024-02-08 19:02:45 -0500

    Navbar added to all pages

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,8 +1,8 @@
 
 export default function Resources() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Resources Page
+    <main className="">
+      <Navbar homepage={false} />
     </main>
   );
 }
