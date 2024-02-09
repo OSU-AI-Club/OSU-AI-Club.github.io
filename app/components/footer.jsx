6fa4d9236beb59eb1c9cc240928d84ea485baf89
Benjamin-Lampe <lampemen@gmail.com>
2024-02-08 20:24:01 -0500

    Prototype footer.

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,5 +1,35 @@
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+
 export default function Footer() {
     return (
-        <div>This is the footer with an update</div>
+        <footer class="min-w-screen-xl bg-sky-600 max-h-screen/16 relative">
+            <div class="flex flex-row w-screen">
+
+                {/* Club Logo */}
+                <a href="/" class="basis-1/4 flex items-center space-x-3 rtl:space-x-reverse justify-center">
+                    <Image src="/logos/AI_Logo_Final.png" width="80" height="80" />
+                </a>
+
+                {/* Info Row */}
+                <div class="basis-1/2 flex flex-col items-center">
+                    <h1 class="text-2xl text-white mt-2">Baker Systems 198</h1>
+                    <h1 class="text-2xl text-white mt-2">Wednesdays at 7:00pm</h1>
+                    <div class="flex flex-row mt-2 mb-2">
+                        <button><FontAwesomeIcon icon={faInstagram} className = "text-white size-7 mr-3" /></button>
+                        <button><FontAwesomeIcon icon={faLinkedin} className = "text-white size-7 mr-3" /></button>
+                        <button><FontAwesomeIcon icon={faDiscord} className = "text-white size-7 mr-3" /></button>
+                        <button><FontAwesomeIcon icon={faNewspaper} className = "text-white size-7 mr-3" /></button>
+                    </div>
+                </div>
+
+                {/* Dead-space since I'm terrible at css */}
+                <div class="basis-1/4 flex flex-row items-center">
+                    
+                </div>
+            </div>
+        </footer>
     )
 }
