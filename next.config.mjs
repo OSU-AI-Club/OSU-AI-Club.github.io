ad2008ab737ea534fd293a54e7d0a02bc6034f97
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2024-03-15 20:52:54 +0100

    Removed base path addition. Creates todo for next deploy.

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,6 @@ const nextConfig = {
   output: "export",
 
   /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/out",
-
-  /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
